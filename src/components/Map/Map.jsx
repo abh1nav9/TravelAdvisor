4b6d40d954ea13f8cb4ea3eb3cc760430fbb5c6f
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-04 23:17:56 +0530

    New Commits

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import googleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutLinedIcon from '@material-ui/icons/LocationCityOutlined'; 
+import { Rating } from '@material-ui/lab';
+import useStyles from './styles';
 
 function Map() {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact>
+        
+      </GoogleMapReact>
+    </div>
   );
 }
 
